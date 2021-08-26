3190f799c734e1226e8c1c44d3d9f290eaad17a6
AAB-87 <andrej-abrekov@yandex.ru>
2021-08-27 00:12:46 +0400

    added tests about Volume

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -36,53 +36,45 @@ public class Radio {
     }
 
 
-//    private int currentStation;
-//    private int maxStation = 9;
-//    private int minStation = 0;
-//
-//    public int getCurrentStation() {
-//        return currentStation;
-//    }
-//
-//    public int getMinStation() {
-//        return minStation;
-//    }
-//
-//    public int getMaxStation() {
-//        return maxStation;
-//    }
-//
-//    public void NextStation() {
-//        if (currentStation == maxStation) {
-//            this.currentStation = minStation;
-//        }
-//        this.currentStation = currentStation + 1;
-//    }
-//
-//    public void PrevStation() {
-//        if (currentStation == minStation) {
-//            this.currentStation = maxStation;
-//        }
-//        this.currentStation = currentStation - 1;
-//    }
-//
-//    public void setCurrentStation(int currentStation) {
-//        if (currentStation > maxStation) {
-//            return;
-//        }
-//        if (currentStation < minStation) {
-//            return;
-//        }
-//        this.currentStation = currentStation;
-//    }
-//
-//    public void setMaxStation(int maxStation) {
-//        this.maxStation = maxStation;
-//    }
-//
-//    public void setMinStation(int minStation) {
-//        this.minStation = minStation;
-//    }
+    private int currentStation;
+    private int maxStation = 9;
+    private int minStation = 0;
 
+    public int getCurrentStation() { return currentStation; }
 
+    public int getMinStation() { return minStation; }
+
+    public int getMaxStation() { return maxStation; }
+
+    public void NextStation(int newCurrentStation) {
+        if (newCurrentStation == maxStation) {
+            this.currentStation = currentStation + 1;
+        }
+        newCurrentStation = minStation;
+    }
+
+    public void PrevStation(int newCurrentStation) {
+        if (newCurrentStation == minStation) {
+            this.currentStation = currentStation - 1;
+        }
+        newCurrentStation = maxStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -80,26 +80,73 @@ class RadioTest {
     }
 
 
-//    @Test
-//    public void ShouldSetNextStation() {
-//        Radio rad = new Radio();
-//        rad.setNextStation();
-//
-//        int expected = rad.getCurrentStation();
-//
-//        int actual = rad.getCurrentStation();
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void ShouldSetPrevStation() {
-//        Radio rad = new Radio();
-//        rad.setPrevStation();
-//
-//        int expected = rad.getCurrentStation();
-//
-//        int actual = rad.getCurrentStation();
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    void shouldNotSetStationBelowMin() {
+        Radio rad = new Radio();
 
+        int expected = rad.getCurrentStation();
+        rad.setCurrentStation(rad.getMinStation() - 12);
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSetStationAboveMax() {
+        Radio rad = new Radio();
+
+        int expected = rad.getCurrentStation();
+        rad.setCurrentStation(rad.getMaxStation() + 12);
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+   @Test
+    public void turnUpNextStation() {
+        Radio rad = new Radio();
+        rad.setCurrentStation(rad.getMaxStation());
+
+        rad.NextStation(10);
+
+        int expected = rad.getMaxStation();
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+  }
+
+   @Test
+   public void turnDownPrevStation() {
+       Radio rad = new Radio();
+        rad.setCurrentStation(rad.getMinStation());
+
+        rad.PrevStation(-1);
+
+        int expected = rad.getMinStation();
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+   }
+
+    @Test
+    void shouldSetMinStation() {
+        Radio rad = new Radio();
+
+        int expected = rad.getCurrentStation();
+        rad.setMinStation(0);
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSetMaxStation() {
+        Radio rad = new Radio();
+
+        int expected = rad.getCurrentStation();
+        rad.setMaxStation(23);
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
 }
