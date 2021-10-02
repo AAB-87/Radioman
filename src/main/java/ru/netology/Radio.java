468697a01b09da164f023ef0067d52a9e5144c6c
AAB-87 <andrej-abrekov@yandex.ru>
2021-10-02 19:35:59 +0400

    Fixed all code

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -6,33 +6,26 @@ public class Radio {
     private int maxVolume = 100;
     private int minVolume = 0;
     private int currentStation;
-//    private int maxStation = 9;
     private int minStation = 0;
     private int numberOfStations = 10;
 
+    public Radio() { }
 
-    public Radio() {
+    public Radio(int numberOfStations) {
+        this.numberOfStations = numberOfStations;
     }
 
-    public int getMaxVolume() { return maxVolume; }
+    public int getMaxVolume() {
+        return maxVolume;
+    }
 
-    public int getMinVolume() { return minVolume; }
+    public int getMinVolume() {
+        return minVolume;
+    }
 
-    public int getCurrentVolume() { return currentVolume; }
-
-//    public void setMaxVolume(int maxVolume) { this.maxVolume = maxVolume; }
-//
-//    public void setMinVolume(int minVolume) { this.minVolume = minVolume; }
-
-//    public void setCurrentVolume(int currentVolume) {
-//        if (currentVolume > maxVolume) {
-//            return;
-//        }
-//        if (currentVolume < minVolume) {
-//            return;
-//        }
-//        this.currentVolume = currentVolume;
-//    }
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 
     public void setCurrentVolume(int currentVolume) {
         if (currentVolume < minVolume) {
@@ -58,26 +51,20 @@ public class Radio {
         this.currentVolume = currentVolume - 1;
     }
 
-    public int getCurrentStation() { return currentStation; }
-
-    public int getMinStation() { return minStation; }
-
-//    public int getMaxStation() { return maxStation; }
-
-//    public void setMaxStation(int maxStation) { this.maxStation = maxStation; }
-
-//    public void setMinStation(int minStation) { this.minStation = minStation; }
-
-    public int getNumberOfStations() { return numberOfStations; }
-
-
-    public Radio(int numberOfStations) {
-        this.numberOfStations = numberOfStations;
+    public int getCurrentStation() {
+        return currentStation;
     }
 
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getNumberOfStations() {
+        return numberOfStations;
+    }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation <= minStation) {
+        if (currentStation < minStation) {
             currentStation = numberOfStations;
         }
         if (currentStation >= numberOfStations) {
@@ -94,6 +81,6 @@ public class Radio {
         if (currentStation == minStation) {
             this.currentStation = numberOfStations;
         }
-            this.currentStation = currentStation - 1;
-        }
+        this.currentStation = currentStation - 1;
     }
+}
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -12,16 +12,7 @@ class RadioTest {
 
         assertEquals(3, rad.getNumberOfStations());
     }
-//
-//    @Test
-//    public void shouldTheNumberOfStations() { // Новая версия
-//        Radio rad = new Radio(9);
-//        rad.setCurrentStation(9);
-//
-//        assertEquals(0, rad.getCurrentStation());
-//    }
-//
-//
+
     @Test
     public void shouldNotSetVolumeBelowMin() { // Громкость не устанавливается ниже минимальной
         Radio rad = new Radio();
@@ -41,14 +32,12 @@ class RadioTest {
         rad.setCurrentVolume(rad.getMaxVolume() + 12);
         int actual = rad.getCurrentVolume();
 
-
         assertEquals(expected, actual);
     }
 
     @Test
     public void turnUpVolume() { // Увеличить громкость
         Radio rad = new Radio();
-        rad.setCurrentVolume(0);
 
         rad.increaseTheVolume();
 
@@ -61,6 +50,7 @@ class RadioTest {
     @Test
     public void turnUpVolume1() { // Увеличить громкость
         Radio rad = new Radio();
+
         rad.setCurrentVolume(99);
 
         rad.increaseTheVolume();
@@ -74,6 +64,7 @@ class RadioTest {
     @Test
     public void turnUpVolume2() { // Увеличить громкость
         Radio rad = new Radio();
+
         rad.setCurrentVolume(100);
 
         rad.increaseTheVolume();
@@ -87,7 +78,6 @@ class RadioTest {
     @Test
     public void turnDownVolume() { // Уменьшить громкость
         Radio rad = new Radio();
-        rad.setCurrentVolume(0);
 
         rad.decreaseTheVolume();
 
@@ -100,6 +90,7 @@ class RadioTest {
     @Test
     public void turnDownVolume1() { // Уменьшить громкость
         Radio rad = new Radio();
+
         rad.setCurrentVolume(100);
 
         rad.decreaseTheVolume();
@@ -113,6 +104,7 @@ class RadioTest {
     @Test
     public void turnDownVolume2() { // Уменьшить громкость
         Radio rad = new Radio();
+
         rad.setCurrentVolume(30);
 
         rad.decreaseTheVolume();
@@ -150,32 +142,7 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
-//
-//    @Test
-//    public void shouldSetTurnDownMinVolume() { // Установка минимальной громкости
-//        Radio rad = new Radio();
-//
-//        int expected = rad.getCurrentVolume();
-//        rad.setMinVolume(0);
-//        int actual = rad.getCurrentVolume();
-//
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldSetTurnUpMaxVolume() { // Установка максимальной громкости
-//        Radio rad = new Radio();
-//
-//        int expected = rad.getCurrentVolume();
-//        rad.setMaxVolume(23);
-//        int actual = rad.getCurrentVolume();
-//
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//
+
     @Test
     public void shouldNotSetStationBelowMin() { // Невозможность установки станции ниже минимальной
         Radio rad = new Radio();
@@ -197,34 +164,6 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
-//
-//   @Test
-//    public void CurrentStationNotEqualToMaxStation() { // Текущая станция не равна максимальной
-//        Radio rad = new Radio();
-//
-//        rad.setCurrentStation(rad.getMinStation());
-//
-//        rad.nextStation();
-//
-//        int expected = rad.getMinStation();
-//        int actual = rad.getCurrentStation() - 1;
-//
-//        assertEquals(expected, actual);
-//  }
-//
-//    @Test
-//    public void CurrentStationNotEqualToMinStation() { // Текущая станция не равна минимальной
-//        Radio rad = new Radio();
-//
-//        rad.setCurrentStation(rad.getNumberOfStations());
-//
-//        rad.prevStation();
-//
-//        int expected = rad.getNumberOfStations();
-//        int actual = rad.getCurrentStation() + 1;
-//
-//        assertEquals(expected, actual);
-//    }
 
     @Test
     public void turnUpNextStation() { // Переключение на следующую станцию с 9 на 0
@@ -258,8 +197,6 @@ class RadioTest {
     public void turnUpNextStation2() { // Переключение на следующую станцию с 0 на 1
         Radio rad = new Radio();
 
-        rad.setCurrentStation(0);
-
         rad.nextStation();
 
         int expected = 1;
@@ -269,14 +206,12 @@ class RadioTest {
     }
 
     @Test
-    public void turnDownPrevStation() { // Переключение на следующую станцию с 9 на 8
-        Radio rad = new Radio();
-
-        rad.setCurrentStation(9);
+    public void turnDownPrevStation() { // Переключение на следующую станцию с 20 на 19
+        Radio rad = new Radio(20);
 
         rad.prevStation();
 
-        int expected = 8;
+        int expected = 19;
         int actual = rad.getCurrentStation();
 
         assertEquals(expected, actual);
@@ -309,30 +244,4 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
-
-
-
-
-//
-//    @Test
-//    public void shouldSetMinStation() { // Установка минимальной станции
-//        Radio rad = new Radio();
-//
-//        int expected = rad.getCurrentStation();
-//        rad.setMinStation(0);
-//        int actual = rad.getCurrentStation();
-//
-//        assertEquals(expected, actual);
-//    }
-
-//    @Test
-//    public void shouldSetMaxStation() { // Установка максимальной станции
-//        Radio rad = new Radio();
-//
-//        int expected = rad.getCurrentStation();
-//        rad.setNumberOfStations(9);
-//        int actual = rad.getCurrentStation();
-//
-//        assertEquals(expected, actual);
-//    }
 }
